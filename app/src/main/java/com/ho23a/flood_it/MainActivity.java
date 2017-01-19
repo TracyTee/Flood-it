9e4e413baca0a7531d157b5a5c6d7c9e8ed183de
TracyTee <savaitracy@gmail.com>
2017-01-18 21:58:28 -0500

    Implemented setting the number of Steps

--- app/src/main/java/com/ho23a/flood_it/MainActivity.java
+++ app/src/main/java/com/ho23a/flood_it/MainActivity.java
@@ -12,6 +12,11 @@ import org.w3c.dom.Text;
 import java.util.HashMap;
 import java.util.Set;
 
+import static com.ho23a.flood_it.Level.DEFAULT;
+import static com.ho23a.flood_it.Level.EASY;
+import static com.ho23a.flood_it.Level.HARD;
+import static com.ho23a.flood_it.Level.MEDIUM;
+
 public class MainActivity extends AppCompatActivity {
     private TextView stepsText;
     private GridView boardView;
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private HashMap<Level, Integer> levelToSizeMap = new HashMap<>();
 //    private int[] buttonColors;
 
+    private HashMap<Level, Integer> levelToNumSteps = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,13 +44,14 @@ public class MainActivity extends AppCompatActivity {
         stepsText = ((TextView) findViewById(R.id.stepsText));
         boardView = ((GridView) findViewById(R.id.boardView));
 
-        levelToSizeMap.put(Level.DEFAULT, 2);
-        levelToSizeMap.put(Level.EASY, 2);
+        levelToSizeMap.put(DEFAULT, 2);
+        levelToSizeMap.put(EASY, 2);
         levelToSizeMap.put(Level.MEDIUM, 4);
         levelToSizeMap.put(Level.HARD, 8);
 
         createBoardView();
         createButtons();
+        setNumSteps();
     }
 
     private void createBoardView() {
@@ -56,4 +63,13 @@ public class MainActivity extends AppCompatActivity {
     private void createButtons() {
     }
 
+    
+    private void setNumSteps(){
+        levelToNumSteps.put(DEFAULT,4);
+        levelToNumSteps.put(EASY,4);
+        levelToNumSteps.put(MEDIUM,12);
+        levelToNumSteps.put(HARD,16);
+
+
+    }
 }
